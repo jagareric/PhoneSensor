dab2d9b9d3db44584fcd40e30b1ee5f2eee16479
jagareric <64065193+jagareric@users.noreply.github.com>
2020-05-02 22:28:28 +0800

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -3,47 +3,11 @@ var pointNum = [] // 已绘制点的编号
 var currentNum // 当前点的编号
 var currentPoint = [] //当前点
 var testPoint //目标点
-traceData = [
-]
-// 设定目标点
-testPoint =
-    [
-        [5.0, 5.0],
-        [5.0, 8.0],
-        [2.0, 5.0],
-        [2.0, 2.0]
-    ]
-function addData(){
-    // 异步加载数据
-    $.getJSON('get_location/').done(function (data) {
-        // 填入更新的位置数据
-        // 当前假定json中只有一个点
-        currentNum = data.number
-        // 未画过新点时才画
-        if (!pointNum.includes(currentNum)){
-            console.log(String(currentNum) + ' is drawing')
-            currentPoint = data.location
-            traceData.push(currentPoint)
-            console.log('Trace is ' +　String(traceData))
-            console.log('currentPoint' +　[currentPoint])
-            pointNum.push(data.number)
-            myChart.setOption({
-                series:
-                    [
-                        {
-                            // 根据名字对应到相应的系列
-                            name: '轨迹',
-                            data: traceData
-                        },
-                        {
-                            // 根据名字对应到相应的系列
-                            name: '当前位置',
-                            data: [currentPoint]
-                        }]
-            });
-        }
-    });
-}
+traceData = []
+// 目标点数组，只含x,y
+testPoint = []
+// 目标点数组，含x,y,z
+targetList = []
 
 async function addData2(data){
     // 异步加载数据
@@ -52,13 +16,21 @@ async function addData2(data){
     currentNum = data['number']
     // 未画过新点时才画
     if (!pointNum.includes(currentNum)){
-        console.log(String(currentNum) + ' is drawing')
-        // currentPoint = data['location']
 
+        console.log('id:' + String(currentNum) + ' is drawing')
+        // currentPoint = data['location']
+        let temp = data['location']
+        temp[2] = temp[2] + parseFloat(beginHeight)
+        $("#currentX").val(String(temp[0]));
+        $("#currentY").val(String(temp[1]));
+        $("#currentZ").val(String(temp[2]));
+        // console.log('当前显示坐标为 ' +　String(temp))
+        currentPoint = [temp[0], temp[1]]
         traceData.push(currentPoint)
-        console.log('Trace is ' +　String(traceData))
-        console.log('currentPoint' +　[currentPoint])
-        pointNum.push(data.number)
+        // console.log('Trace is ' +　String(traceData))
+        console.log('currentPoint' +　' (' + [currentPoint] + ')')
+        calcNext(targetList,temp)
+        pointNum.push(currentNum)
         myChart.setOption({
             series:
                 [
@@ -88,16 +60,17 @@ var option = {
             legend: {
                 data: [{
                     name: '轨迹',
-                    icon: 'circle'
-                },
+                    },
                     {
                         name: '当前位置',
-                        icon: 'diamond'
                     },
                     {
                         name: '布设目标',
-                        icon: 'pin'
-                    }]
+                    },
+                    {
+                        name: '最近路径'
+                    }
+                ]
             },
             // 悬停时显示点坐标
             tooltip: {
@@ -129,64 +102,20 @@ var option = {
                         symbolSize: 20,
                         symbol: 'pin',
                         color: '#CC3333',
-                        data: [
-                            [5.0, 5.0],
-                            [5.0, 8.0],
-                            [2.0, 5.0],
-                            [2.0, 2.0]
-                        ],
+                        data: [],
                         name:'布设目标',
                         type: 'scatter'
+                    },
+                    {
+                        symbol:'circle',
+                        symbolSize: 5,
+                        color: '#90B44B',
+                        data:[],
+                        name:'最近路径',
+                        type: 'line'
                     }
                 ]
         }
-    // },
-    // 实现响应式绘图,控制各种情况下的缩放比例，元素布局方式，还没搞定
-    //     media: [ // 这里定义了 media query 的逐条规则。
-    //         {
-    //             query: { minAspectRatio: 1},   // 第一个规则，长宽比大于等于1时
-    //             option: {       // 第一个规则满足下的option。
-    //                 series: [
-    //                     {
-    //                         radius: [20, '50%'],
-    //                         center: ['25%', '50%']
-    //                     }
-    //                 ]
-    //             }
-    //         },
-    //         {
-    //             query: { maxAspectRatio: 1},   // 第二个规则，长宽比小1时
-    //             option: {
-    //                 series: [
-    //                     {
-    //                         radius: [20, '50%'],
-    //                         center: ['50%', '30%']
-    //                     }
-    //                 ]
-    //             }
-    //         },
-    //         {
-    //             query: { maxWidth: 500},   // 第二个规则，宽度小于500像素时
-    //             option: {
-    //                 series: [
-    //                     {
-    //                         radius: [20, '50%'],
-    //                         center: ['50%', '30%']
-    //                     }
-    //                 ]
-    //             }
-    //         },
-    //         {                   // 这条里没有写规则，表示『默认』，
-    //             option: {       // 即所有规则都不满足时，采纳这个option。
-    //                 series: [
-    //                     {
-    //                         radius: [20, '50%'],
-    //                         center: ['25%', '50%']
-    //                     }
-    //                 ]
-    //             }
-    //         }
-    //     ]
 };
 
 // 使用刚指定的配置项和数据显示图表。
@@ -197,23 +126,14 @@ window.addEventListener('resize', () => {
 })
 
 // 按钮-开始
-var startCode
-var drawInterval
 $("#start").click(function(){
     recordData = true;
     startTime = Date.now();
-    position();
 });
 
 // 按钮-结束
-var finishCode
 $("#finish").click(function(){
     recordData = false;
-    CalPath.insert( { id: idNow + 1, time: 0,
-        ax: 10, ay: 10, az: 10,
-        wx: 10, wy: 10, wz: 10,
-        used: false, calculated: false
-    } )
 });
 
 // 按钮-清除轨迹
@@ -226,3 +146,91 @@ $("#clearTrace").click(function(){
                 data: traceData
             }]
     });});
+
+// 按钮-重置
+$("#reload").click(function(){
+    location.reload(); // 刷新当前页面
+});
+
+// 按钮-设置初始高度
+var beginHeight = 0;
+$("#setHeight").click(function(){
+    let temp = $("#beginHeight").val();
+    if ($.isNumeric(temp)){ // 为数字的时候才继续执行
+        beginHeight = temp;
+    }
+    else {
+        console.log('输入数据非数字');
+    }
+    console.log('初始高度为' + String(beginHeight))
+});
+
+// 按钮-设置目标
+$("#setTarget").click(function(){
+    let targetX,targetY,targetZ
+    targetX = $("#targetX").val()
+    targetY = $("#targetY").val()
+    targetZ = $("#targetZ").val()
+    if ($.isNumeric(targetX)&&$.isNumeric(targetY)&&$.isNumeric(targetZ)){ // 只有全为数字的时候才继续执行
+        let targetAdd = [targetX,targetY]
+        testPoint.push(targetAdd) // 这里抛弃了targetZ
+        let targetAdd2 = [targetX,targetY,targetZ]
+        targetList.push(targetAdd2)
+        console.log('当前增加目标为 ' +　String(targetAdd))
+        console.log('全部目标为 ' +　String(testPoint))
+        myChart.setOption({
+            series:[
+                {
+                    name: '布设目标',
+                    data: testPoint
+                }]
+        });
+    }
+    else {
+        console.log('输入数据非数字')
+    }
+});
+
+// 用于遍历的函数，输入目标坐标和当前坐标，返回差值数组
+function calcDelta(target,current){
+    return [target[0]-current[0],target[1]-current[1],target[2]-current[2]];
+}
+
+// 用于求距离的函数，输入差值数组，返回绝对值之和
+function calcDistance(delta) {
+    return Math.abs(delta[0]) + Math.abs(delta[1]) + Math.abs(delta[2]);
+}
+
+// 遍历目标点数组，求差值用于求最近路径和调整量，用于定时器调用或画图时调用
+// current应是加了初始高度的
+async function calcNext(targetList,current) {
+    let deltaList = [];
+    let distanceList = [];
+    if(targetList.length !== 0){
+        console.log('求最近距离！')
+        for(let i in targetList) {
+            let temp = calcDelta(targetList[i],current);
+            deltaList.push(temp)
+            distanceList.push(calcDistance(temp))
+        }
+        let min = Math.min.apply(null, distanceList);
+        let nextIndex = distanceList.indexOf(min); // 求出最小距离的索引
+        let deltaNext = deltaList[nextIndex]
+        console.log('最小距离为：' +　String(min))
+        console.log('最近点为：' +　String(targetList[nextIndex]))
+        console.log('delta为：' +　String(deltaNext))
+        $("#deltaX").val(String(deltaNext[0]));
+        $("#deltaY").val(String(deltaNext[1]));
+        $("#deltaZ").val(String(deltaNext[2]));
+        myChart.setOption({
+            series:[
+                {
+                    name: '最近路径',
+                    data: [
+                        [targetList[nextIndex][0],targetList[nextIndex][1]],
+                        [current[0],current[1]]
+                    ]
+                }]
+        });
+    }
+}
--- scripts/position.js
+++ scripts/position.js
@@ -1,129 +1,117 @@
-// array.splice(第几个数开始，删除几个数，要添加的数据)
-async function position()
-{
-    var i = 2, s = 50, q0= [[1],[0],[0],[0]], v0 = [[0],[0],[0]], s0 = [[0],[0],[0]];
-    var H0= [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
-    var q1, v1, s1, H1;
-    // 下标1表示是当前值，下标0存储的上一时刻点的值
-    // i是对当前进行到第几个点的标号 s是由于需要求加速度的前后各s个点的方差
+// 下标1表示是当前值，下标0存储的上一时刻点的值
+q0 = q1 = [[1],[0],[0],[0]];
+v0 = v1 = [0,0,0];
+s0 = s1 = [0,0,0];
+H0 = H1 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+
+function position(data_pre, data_now, data_post){
+    // 调用本函数的时候就已经从内存数据库中提取我们所存储的数据
+    // data_now表示当前时间点 data_pre表示前五十个点的数据，同理data_post为后五十个点，总共101个点。
+    // console.log(s0);
+    // console.log(s0[1]);
+    // console.log(s0[2]);
+    // console.log(s1);
+    // console.log(s1[1]);
+    // console.log(s1[2]);
+    // console.log(v0);
+    // console.log(v0[1]);
+    // console.log(v0[2]);
+    // console.log(v1);
+    // console.log(v1[1]);
+    // console.log(v1[2]);
     var id, ax, ay, az, wx, wy, wz, T;
     var returnData;
-    while(1)
-    {
-        // TODO:数据读取的部分暂时还空着，假设这个部分已经正常读取了
-        var data0 = CalPath.chain().where(function (obj) { return obj.id === (i-1) }).data()[0];
-        var data1 = CalPath.chain().where(function (obj) { return obj.id === i }).data()[0];
-        // 这里是再从内存数据库中提取我们所存储的数据，data1表示当前时间点 data0表示上一时间点。
-        id = data1.id;
-        wx = data1.wx;
-        wy = data1.wy;
-        wz = data1.wz;
-        ax = data1.ax;
-        ay = data1.ay;
-        az = data1.az;
-        T = data1.time - data0.time;
+    // 在数据库里面的数据量足够的情况下提取数据，否则会出现读取错误
+    id = data_now.id;
+    wx = data_now.wx;
+    wy = data_now.wy;
+    wz = data_now.wz;
+    ax = data_now.ax;
+    ay = data_now.ay;
+    az = data_now.az;
+    T = (data_now.time - data_pre[49].time) * 0.001;
+    // data_pre[49]为当前时刻的上一时刻点
+    var c = zero_judge(data_pre, data_now, data_post);
 
-        if (wx === 10 && wy === 10 && wz === 10 && ax === 10 &&  ay === 10 && az === 10)
-        {
-            break;
-            //这里是选了一个平时不可能达到的各个加速度和角度度值来作为数据的结尾
-        }
-        var C = zero_judge(s, i, ax, ay, az, wx, wy, wz);
-        if (C===0)
-        {
-            i = i + 1;
-            wx = wy = wz = ax = ay = az = 0;
-            v1 = v0;
-            data1.vx = v1[0];
-            data1.vy = v1[1];
-            data1.vz = v1[2];
-            data1.sx = s0[0]+v1[0]*T;
-            data1.sy = s0[1]+v1[1]*T;
-            data1.sz = s0[2]+v1[2]*T;
-            // 判断为0加速度，则保持原速度不变前进T秒，数据存入
-            continue;
-        }
-        H1 = [[0,-wx,-wy,-wz],[wx,0,wz,-wy],[wy,-wz,0,wx],[wz,wy,-wx,0]];
-        // H矩阵为角速度所构成的四维角速度矩阵
-        q1 = q1_cal(q0, H0, H1, T);
-        // q即为四元数矩阵4*1
-        var obj = pos_cal(ax, ay, az, T, q1, v0, s0);
-        data1.sx = obj.s1;
-        data1.sy = obj.s2;
-        data1.sz = obj.s3;
-        data1.vx = obj.v1;
-        data1.vy = obj.v2;
-        data1.vz = obj.v3;
-        // 此处把数据写入进数据库中，然后重置此处数组
-        returnData = {number: id, location:[obj.s1,obj.s2,obj.s3]};
+    if(c===0){  // 0加速度条件下
+        wx = wy = wz = ax = ay = az = 0;
+        // 判断为0加速度，则保持原速度不变前进T秒
+        s1[0] = s0[0] + v0[0] * T;
+        s1[1] = s0[1] + v0[1] * T;
+        s1[2] = s0[2] + v0[2] * T;
+        // 画图
+        returnData = {number: id, location:[s1[0],s1[1],s1[2]]}
         addData2(returnData);
-        // 调用画图函数
+        s0 = s1;
+        q0 = [[1],[0],[0],[0]];
+        H0 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];  // 重置数据
+    }
+
+    if(c===1){// 非0加速度条件下
+        H1 = [[0,-wx,-wy,-wz],[wx,0,wz,-wy],[wy,-wz,0,wx],[wz,wy,-wx,0]];  // 四维角速度矩阵
+        q1 = q1_cal(T);  // 四元数矩阵4*1
+        pos_cal(ax, ay, az, T);  // 把数据写入进数据库中，然后重置此处数组
+        returnData = {number: id, location:[s1[0],s1[1],s1[2]]};
+        // console.log(s1[0],s1[1],s1[2]);
+        addData2(returnData);  // 调用画图函数
+        // 重置数据
         v0 = v1;
         s0 = s1;
         H0 = H1;
         q0 = q1;
-        i = i+1;
-        // 重置数据
-
     }
 }
 
-function zero_judge(s, i, ax, ay, az, wx, wy, wz)
-{
-    th=0.01;
-    var a_var, data2, am=[], c=1;
-    a = math.sqrt(ax ** 2 + ay ** 2 + az ** 2);
-    w = math.sqrt(wx ** 2 + wy ** 2 + wz ** 2);
-    // a和w就是当前点的加速度和角速度幅值
-    if( a < th && w < th)
-    {
-        if(s<i)
-        {
-            // TODO: 采集前后共2s+1组数据,求加速度方差
-            data2 = CalPath.chain().where(function (obj) { return obj.id >= i-s && obj.id <= i+s }).data();
-            for(k=0; k<2*s+1; k++)
-            {
-                am[k] = math.sqrt(data2[k].ax ** 2 + data2[k].ay ** 2 + data2[k].az ** 2);
-            }
-            a_var = math.variance(am);
-            //求方差
+function zero_judge(data_pre, data_now, data_post){
+    var th1=0.5, th2=0.3, s=50;
+    var a_var, am=[], c=1;
+    a = math.sqrt(data_now.ax ** 2 + data_now.ay ** 2 + data_now.az ** 2);
+    w = math.sqrt(data_now.wx ** 2 + data_now.wy ** 2 + data_now.wz ** 2);
+    am[s] = a;
+    // a和w为当前点的加速度和角速度幅值
+    if( a < th1 && w < th2){
+        // 采集前后共2s+1组数据,求加速度方差
+        // 提取后前十个数据
+        for(k=0; k<s; k++){
+            am[k] = math.sqrt(data_pre[k].ax ** 2 + data_pre[k].ay ** 2 + data_pre[k].az ** 2);
         }
-        else
-        {
-            // TODO: 采集s+i组数据,求加速度方差
-            data2 = CalPath.chain().where(function (obj) { return obj.id >= 1 && obj.id <= i+s }).data();
-            for(k=0; k<s+i; k++)
-            {
-                am[k] = math.sqrt(data2[k].ax ** 2 + data2[k].ay ** 2 + data2[k].az ** 2);
-            }
-            a_var = math.variance(am);
-            //求方差
+        // 提取后五十个数据
+        for(k=0; k<s; k++){
+            am[k+51] = math.sqrt(data_post[k].ax ** 2 + data_post[k].ay ** 2 + data_post[k].az ** 2);
         }
-        if(a_var < th)
+        //求方差
+        a_var = math.variance(am);
+        if(a_var < th1)
         {
             c = 0;
         }
     }
     return c;
 }
-// 这个函数是计算q1四元数矩阵的
-function q1_cal(q0, H0, H1, T)
-{
+
+// 计算q1四元数矩阵
+function q1_cal(T){
     var zj = math.multiply(T, H0, q0);
     zj = math.multiply(H1,math.add(q0, zj));
     zj = math.multiply(0.5*T, math.add(math.multiply(H0, q0),zj));
     q1 = math.add(q0, zj);
+    q1_all = math.sqrt(q1[0][0] ** 2 + q1[1][0] ** 2 + q1[2][0] ** 2+ q1[3][0] ** 2);
+    q1[0][0] = q1[0][0] / q1_all;
+    q1[1][0] = q1[1][0] / q1_all;
+    q1[2][0] = q1[2][0] / q1_all;
+    q1[3][0] = q1[3][0] / q1_all;
     return q1;
 }
-// 这个函数是通过加速度和时间计算出最终位置信息
-function pos_cal(ax, ay, az, T, q1, v0, s0)
-{
-    var q00, q11, q22, q33, c1, c2, c3, c4, c5, c6, c7, c8, c9, G, V, S, C;
-    q00 = q1[0];
-    q11 = q1[1];
-    q22 = q1[2];
-    q33 = q1[3];
+// 通过加速度和时间计算出最终位置信息
+function pos_cal(ax, ay, az, T){
+    var a, q00, q11, q22, q33, c1, c2, c3, c4, c5, c6, c7, c8, c9, G, V, S, C, g=9.8;
     // q00 q11 q22 q33分别是四元数矩阵中的四个值，单独取出方便计算
+    q00 = q1[0][0];
+    q11 = q1[1][0];
+    q22 = q1[2][0];
+    q33 = q1[3][0];
+
+    // 3*3矩阵中的各个区域的值
     c1 = q00^2+q11^2-q22^2-q33^2;
     c2 = 2*(q11*q22+q00*q33);
     c3 = 2*(q11*q33-q00*q22);
@@ -133,20 +121,14 @@ function pos_cal(ax, ay, az, T, q1, v0, s0)
     c7 = 2*(q11*q33+q00*q22);
     c8 = 2*(q22*q33-q00*q11);
     c9 = q00**2-q11*2-q22**2+q33**2;
-    C = [[c1,c2,c3],[c4,c5,c6],[c7,c8,c9]];
-    // C即为姿态结算矩阵
-    // c1-c9分别是3*3矩阵中的各个区域的值
+    C = [[c1,c2,c3],[c4,c5,c6],[c7,c8,c9]];  // 姿态解算矩阵
     a = [[ax],[ay],[az]];
-    G = [[0],[0],[-g]];
-    a = math.add(math.multiply(C,a),G);
-    // 减去竖直方向上的重力加速度影响
-    V = math.add(v0, math.multiply(a, T));
-    S = math.add(s0, math.multiply(T,v0), math.multiply(0.5, T ** 2, a))
-    sx = S[0];
-    sy = S[1];
-    sz = S[2];
-    vx = V[0];
-    vy = V[1];
-    vz = V[2];
-    return {s1:sx, s2:sy, s3:sz, v1:vx, v2:vy, v3:vz};
+    a = math.multiply(C,a);
+    // a[2][0] = a[2][0] + g;
+    v1[0] = v0[0] + a[0][0] * T;
+    v1[1] = v0[1] + a[1][0] * T;
+    v1[2] = v0[2] + a[2][0] * T;
+    s1[0] = s0[0] + v0[0] * T + 0.5 * T * T * a[0][0];
+    s1[1] = s0[1] + v0[1] * T + 0.5 * T * T * a[1][0];
+    s1[2] = s0[2] + v0[2] * T + 0.5 * T * T * a[2][0];
 }
